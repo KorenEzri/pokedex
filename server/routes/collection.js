23c8ebb3cf0c2251d8c318018ba016ec234e360d
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-03-19 14:43:14 +0200

    finish basic backend endpoints

--- server/routes/collection.js
+++ server/routes/collection.js
@@ -1,27 +1,37 @@
 const { Router } = require("express");
-
 const collection = Router();
+let collectionObjectArray = [];
 
 collection.get("/", (req, res) => {
-  res.send("collection route");
+  res.send(collectionObjectArray);
 });
 
-// pokemon.post("/catch", async (req, res) => {
-//   try {
-//     const pokemonName = req.params.name;
-//     const { data } = await network.get(pokemonDataLink + pokemonName);
-//     const { name, height, weight, type, sprites } = data;
-//     const responseObject = {
-//       name,
-//       height,
-//       weight,
-//       type,
-//       sprites,
-//     };
-//     res.json(`RECEIVED!, ${JSON.stringify(responseObject)}`);
-//   } catch ({ message }) {
-//     console.log(message);
-//   }
-// });
+collection.post("/catch", async (req, res) => {
+  try {
+    const data = req.body;
+    const pokemonName = data.pokemonData.name;
+    collectionObjectArray.push({ id: data.id, pokemonData: data });
+    res.send(
+      `Added ${pokemonName} to your collection! Your collection is: ${JSON.stringify(
+        collectionObjectArray
+      )}`
+    );
+  } catch ({ message }) {
+    console.log(message);
+  }
+});
+
+collection.delete("/release/:id", async (req, res) => {
+  try {
+    const pokemonID = req.params.id;
+    const updatedCollectionArray = collectionObjectArray.filter(
+      (pokemonObject) => pokemonObject.id !== Number(pokemonID)
+    );
+    collectionObjectArray = updatedCollectionArray;
+    res.send(collectionObjectArray);
+  } catch ({ message }) {
+    console.log(message);
+  }
+});
 
 module.exports = collection;
--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -1,15 +1,58 @@
 const { Router } = require("express");
+const fs = require("fs");
 const pokemon = Router();
 const { network, pokeAPI_ROUTES } = require("../utils/pokeAPI");
 pokemon.get("/", (req, res) => {
   res.send("Pokemon route");
 });
 
+// const validatePokemonID = async (pokemonID) => {
+//   const filePath = `./pokemon-ids.json`
+//   // {id: 132, name:ditto }
+//   const storedPokemonIDS = fs.readFile(filePath, `utf8`, (err, data) => {
+//     if(!err){
+//       return data;
+//     } else{
+//       console.log(err);
+//     }
+//     const pokemonIDInStorage  = data.filter((pokeObject =>{
+//       pokeObject.id === pokemonID
+//     })) || false
+//     if (pokemonIDInStorage) {
+
+//     }
+//   })
+// }
+
 pokemon.get("/:name", async (req, res) => {
   try {
     const pokemonName = req.params.name;
     const { data } = await network.get(
       `${pokeAPI_ROUTES.allData}pokemon/${pokemonName}`
+    );
+    const { name, height, weight, type, sprites, id } = data;
+    const responseObject = {
+      name,
+      height,
+      weight,
+      type,
+      id,
+      pictures: {
+        front: sprites.front_default,
+        back: sprites.back_default,
+      },
+    };
+    res.send(responseObject);
+  } catch ({ message }) {
+    console.log(message);
+  }
+});
+
+pokemon.get("/:id", async (req, res) => {
+  try {
+    const pokemonID = req.params.id;
+    const { data } = await network.get(
+      `${pokeAPI_ROUTES.allData}pokemon/${pokemonID}`
     );
     const { name, height, weight, type, sprites, id } = data;
     const responseObject = {
