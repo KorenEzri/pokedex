e048a853ebbd1dbaf308efcb9c0f914f5fef23b9
Koren <73742515+KorenEzri@users.noreply.github.com>
2021-03-19 21:41:19 +0200

    Update index.js

--- client/src/components/PokemonPresentor/index.js
+++ client/src/components/PokemonPresentor/index.js
@@ -2,26 +2,18 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 export default function PokemonPresentor({ pokemonData }) {
-  // const frontOrBack = (bool) => {
-  //   if (bool) {
-  //     return pokemonData.pictures.back;
-  //   } else {
-  //     return pokemonData.pictures.front;
-  //   }
-  // };
   return (
     <div className="pokemon-wrapper">
       <div className="name">{pokemonData.name}</div>
-      <div
-      // onMouseIn={() => {
-      //   frontOrBack(false);
-      // }}
-      // onMouseOut={() => {
-      //   setPokemonPicture(false);
-      // }}
-      >
-        {/* <img src={frontOrBack(pokemonPicture)} /> */}
-      </div>
+      <img
+        onMouseOver={(e) => {
+          e.target.src = `${pokemonData.pictures.back}`;
+        }}
+        onMouseOut={(e) => {
+          e.target.src = `${pokemonData.pictures.front}`;
+        }}
+        src={pokemonData.pictures.front}
+      />
       <div>height:{pokemonData.height}</div>
       <div>weight:{pokemonData.weight}</div>
     </div>
